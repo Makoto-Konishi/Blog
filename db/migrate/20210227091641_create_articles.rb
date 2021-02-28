e0b41f618566c1a56fd33febe58f5e69e8ea9c58
Makotox0207 <xiaoxishicheng@gmail.com>
2021-02-28 20:22:08 +0900

    add_delete_and_edit_to_show

--- db/migrate/20210227091641_create_articles.rb
+++ db/migrate/20210227091641_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title, null: false
       t.text :text, null: false
-
+      
       t.timestamps
     end
   end
