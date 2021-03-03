3dc6438046fb00c605dd810871244b8e5454c6f5
Makotox0207 <xiaoxishicheng@gmail.com>
2021-03-04 02:34:08 +0900

    add error message to login form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 20}
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
-  validates :password_confirmation, presence: true
 
   has_secure_password 
 end
