cb47ac5e32ade055c2500d9aedfe495d3075c4ad
Makotox0207 <xiaoxishicheng@gmail.com>
2021-03-06 20:50:01 +0900

    add text to article's show view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/signup',to: 'users#new'
-  root to: 'users#index'
   resources :users, except: [:new]
   resources :articles
 end
